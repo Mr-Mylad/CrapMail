2150debfb2cdef793ac879854a0a11fae8df93a0
Mr. Mylad <mrmylad10@gmail.com>
2023-04-21 17:11:07 +0530

    Initialized project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5hEjPS9f5irx9X2mVMpfnwk3Q64_36kA",
+  authDomain: "crapmail-93713.firebaseapp.com",
+  projectId: "crapmail-93713",
+  storageBucket: "crapmail-93713.appspot.com",
+  messagingSenderId: "848517842264",
+  appId: "1:848517842264:web:44cd4f352f627b4ad2912d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
