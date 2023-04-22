f56b4f91fd8b7a12712157977a865f8b0463dfd0
Mr. Mylad <mrmylad10@gmail.com>
2023-04-22 12:48:29 +0530

    Added firestore reference and a UsersCollection reference

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { 
+    getFirestore,
+    collection
+} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const firestore = getFirestore(app);
+
+// Initialize Users collection
+const UsersCollection = collection(firestore, "Users");
