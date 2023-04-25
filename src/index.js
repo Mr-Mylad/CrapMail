e7bbda3015fb331033a279ddbeb266181003511b
Mr. Mylad <mrmylad10@gmail.com>
2023-04-25 20:28:55 +0530

    Imported doc from firebase

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import {
     getDocs,
     addDoc,
     onSnapshot,
+    doc
 } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
